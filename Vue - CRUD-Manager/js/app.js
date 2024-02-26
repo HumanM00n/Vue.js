3c3f7c0612cbfdcad91e0f6c4e27b6e299bade4f
HumanM00n <113510790+HumanM00n@users.noreply.github.com>
2024-02-26 16:27:41 +0100

    Add files via upload

---
+++ Vue - CRUD-Manager/js/app.js
@@ -0,0 +1,19 @@
+const App = {
+
+    data() {
+
+        return {
+            message: "Hola que tal"
+        }
+    },
+    methods: {
+
+    },
+
+    mounted() { /* Méthode du cycle de vie de notre objet */
+    
+    },
+}
+
+
+Vue.createApp(App).mount("#app")
