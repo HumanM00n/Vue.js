cc1ba4503576526d3921175273a2bb2d96a9706d
HumanM00n <113510790+HumanM00n@users.noreply.github.com>
2024-02-27 17:39:35 +0100

    Update app.js

--- Vue - CRUD-Manager/js/app.js
+++ Vue - CRUD-Manager/js/app.js
@@ -3,12 +3,61 @@ const App = {
     data() {
 
         return {
-            /* Création de variable */ 
+            /* CREATION DES VARIABLES */
             showHome: false,
-            showCreateForm: true,
+            showCreateForm: false,
             showStudentsList: false,
         }
     },
+
+    mounted() {
+        this.changeNavigationState("home")
+    },
+
+
+
+    methods: {
+        /* CREATION DES METHODES ==> FONCTIONS */
+        gotoHome() {
+            this.changeNavigationState("home")
+        },
+
+        gotoCreateForm() {
+            this.changeNavigationState("create") // On peut mettre ceci à la place de 'showCreateForm' car 'showCreateForm' est dans la foncion 'changeNavigationState' et la case create 
+        },
+
+        gotoStudentsList() {
+            this.changeNavigationState("list")
+        },
+
+
+
+        changeNavigationState(destination) {
+            this.showCreateForm = false
+            this.showHome = false
+            this.showStudentsList = false
+
+            switch (destination) {
+
+                case "home":
+                    this.showHome = true
+                    break;
+
+                case "create":
+                    this.showCreateForm = true
+                    break;
+                
+                case "list":
+                    this.showStudentsList = true
+                    break;
+
+                default:
+                    this.showHome = true
+
+                    break;
+            }
+        }
+    }
 }
 
 
